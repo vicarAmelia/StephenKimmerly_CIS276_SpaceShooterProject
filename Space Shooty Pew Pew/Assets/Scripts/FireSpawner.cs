43a3541a99f7a943cacc9137c3562c592b74f915
vicarAmelia <98558073+vicarAmelia@users.noreply.github.com>
2022-03-03 17:56:03 -0500

    Got Scripts back in and imported Space shooter assets

---
+++ Space Shooty Pew Pew/Assets/Scripts/FireSpawner.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireSpawner : MonoBehaviour
+{
+    public GameObject FireShot;
+    public GameObject BigShot;
+    // Update is called once per frame
+    void Update()
+    {
+        //Fires main weapon
+        if(Input.GetButtonDown("Fire1"))
+        {
+            GameObject ball;
+            ball = Instantiate(FireShot, transform);
+
+            ball.GetComponent<Rigidbody>().AddForce(Vector3.right * 70f, ForceMode.Impulse);
+        }
+
+        if(Input.GetButton("Fire2"))
+        {
+            GameObject capsule;
+            capsule = Instantiate(BigShot, transform);
+
+            capsule.GetComponent<Rigidbody>().AddForce(Vector3.right * 70f, ForceMode.Impulse);
+        }
+    }
+}
