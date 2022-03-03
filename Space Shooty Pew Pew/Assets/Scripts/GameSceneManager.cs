a1fcac1bef0a4777d02fee4d6788bccaf2c08c61
vicarAmelia <98558073+vicarAmelia@users.noreply.github.com>
2022-03-03 17:19:44 -0500

    Added back more things because last version got borked

---
+++ Space Shooty Pew Pew/Assets/Scripts/GameSceneManager.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameSceneManager : MonoBehaviour
+{
+    public static GameSceneManager Instance;
+    public float uiLoadTime = 0.5f;
+    private AsyncOperation asyncOperation;
+
+    private void Awake()
+    {
+        if(Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+        
+    }
+
+    
+
+    public void LoadScene(string sceneName)
+    {
+        StartCoroutine(LoadNewScene(sceneName));
+    }
+
+    private IEnumerator LoadNewScene(string sceneName)
+    {
+        yield return null;
+        Time.timeScale = 0f;
+
+        asyncOperation = SceneManager.LoadSceneAsync(sceneName);
+        while(!asyncOperation.isDone)
+        {
+            yield return null; //Waits a frame
+        }
+    }
+}
---
+++ Space Shooty Pew Pew/Assets/Scripts/MainMenuUI.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenuUI : MonoBehaviour
+{
+    public Button startButton;
+    public Button exitButton;
+    public string gameSceneName;
+    // Start is called before the first frame update
+    private void Start()
+    {
+       startButton.onClick.AddListener(LoadGameScene); 
+    }
+
+    public void LoadGameScene()
+    {
+        GameSceneManager.Instance.LoadScene(gameSceneName);
+    }
+
+}
